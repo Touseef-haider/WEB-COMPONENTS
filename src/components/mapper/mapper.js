980c5d0185a45374847c184b78c3afb9c3833c42
touseef-haider <touseefhaider355@gmail.com>
2021-07-24 18:35:47 +0500

    chore : done with the coordinates of the image

--- src/components/mapper/mapper.js
+++ src/components/mapper/mapper.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Car from './car.png'
 import ImageMapper from 'react-img-mapper';
 import Area from './area.json'
@@ -8,11 +8,30 @@ const Mapper = () => {
         name: 'my-map',
         // GET JSON FROM BELOW URL AS AN EXAMPLE
         areas: Area,
-      };
+    };
+
+    const handleClick = (value) =>{
+        console.log(value)
+
+    }
+
+    useEffect(()=>{
+
+        document.body.addEventListener('onClick',handleBodyClick)
+        
+        return ()=>{
+            document.body.removeEventListener('onClick',handleBodyClick)
+        } 
+    },[])
+
+    const handleBodyClick = (e) =>{
+        console.log(e)
+    }
+
     return (
         <>
 
-        <ImageMapper src={Car} map={MAP}/> 
+        <ImageMapper src={Car} map={MAP} onClick={(e)=>handleClick(e)}/> 
 
         </>
     )

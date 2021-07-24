0a498045e7cbf4c322cb5da13795a00bf0fc1aae
touseef-haider <touseefhaider355@gmail.com>
2021-07-24 21:29:34 +0500

    chore : problem with image responsiveness

--- src/components/mapper/mapper.js
+++ src/components/mapper/mapper.js
@@ -1,9 +1,12 @@
 import React,{useEffect} from 'react'
 import Car from './car.png'
+import Mark from './mark.svg'
 import ImageMapper from 'react-img-mapper';
 import Area from './area.json'
 
 const Mapper = () => {
+
+const mapperRef = React.useRef(null);   
     const MAP = {
         name: 'my-map',
         // GET JSON FROM BELOW URL AS AN EXAMPLE
@@ -11,37 +14,36 @@ const Mapper = () => {
     };
 
     const handleClick = (value) =>{
-        console.log(value)
-
+        console.log('selected area',value)
+    
     }
 
+
     const handleSpot=(e)=>{
+        console.log('x',e.pageX)
+        console.log('y',e.pageY)
+        if(!e.target.coords) return
         let mouseX = e.pageX;
         let mouseY = e.pageY;
-        var color = '#0057ff';
-
         var size = '13px';
         let div = window.document.createElement('div')
-        div.style.position = 'absolute';
+        div.style.position = 'fixed';
         div.style.top = `${mouseY}px`;
         div.style.left = `${mouseX}px`;
         div.style.height = size;
         div.style.zIndex = 1;
-        div.style.borderRadius = '50%';
         div.style.width = size;
-        div.style.backgroundColor = color;
+        div.style.backgroundImage = `url(${Mark})`
+        div.style.backgroundPosition = `center`
         window.document.body.appendChild(div)
-       
     }
 
     
     return (
-        <>
-
-        <div className="mapper" onClick={handleSpot}>
-            <ImageMapper src={Car} map={MAP} onClick={(e)=>handleClick(e)}/> 
-        </div>
-
+        <>      
+            <div className="mapper" onClick={handleSpot}>
+                <ImageMapper containerRef={mapperRef} src={Car} map={MAP}   onClick={handleClick}/> 
+            </div>
         </>
     )
 

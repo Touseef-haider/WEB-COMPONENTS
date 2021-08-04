2934c5c434d271354cd1b49ae16b33a3c7e9c388
touseef-haider <touseefhaider355@gmail.com>
2021-08-04 18:28:35 +0500

    chore : working on places api

---
+++ src/components/auto-select-area/index.js
@@ -0,0 +1,53 @@
+import React,{useEffect,useState} from 'react';
+import GooglePlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId } from 'react-google-places-autocomplete';
+import Arrow from './drop-down-arrow.png'
+import axios from 'axios'
+
+
+const AreaSelect = () => {
+    const [value, setValue] = useState(null);
+//   useEffect(() =>{
+//     let el = document.querySelector('.css-tlfecz-indicatorContainer')
+//     let img = document.createElement('img')
+//     img.src = `${Arrow}`
+//     img.width='10'
+//     img.height='10'
+//     el.appendChild(img)
+//   },[])
+
+    
+
+
+    const handleChange = (value) =>{
+        console.log(value)
+        geocodeByAddress(value.label)
+        .then(results => console.log(results))
+        .catch(error => console.error(error));
+        setValue()
+        geocodeByPlaceId(value.value.place_id)
+        .then(results => {
+            
+            
+            let lat = results[0].geometry.location.lat()
+            let lng = results[0].geometry.location.lng()
+            // axios.get(`http://api.positionstack.com/v1/forward?access_key=23a79690702e249ff6040ba352164558chore
+            console.log({lat,lng})
+        })
+        .catch(error => console.error(error));
+    }
+
+  return(
+    <div>
+        <GooglePlacesAutocomplete
+            selectProps={{
+                value,
+                onChange: handleChange,
+            }}
+            apiKey="AIzaSyCT8tgryBs-D7w05IHPMmHkFeZwxMfFIuE"
+        />
+    </div>
+  )
+}
+
+
+export default AreaSelect;
